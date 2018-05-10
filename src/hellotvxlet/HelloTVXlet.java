628c29534ae9e709c5b33c92e08c02447bf7c963
Katriena Banasik <katriena.0107@hotmail.com>
2018-05-10 21:22:54 +0200

    init screen text

--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -9,15 +9,24 @@ import java.awt.Color;
  * @author student
  */
 
-public class HelloTVXlet implements Xlet { //implements javax.microedition.xlet.Xlet
+public class HelloTVXlet implements Xlet {
     
     //default constructor without arguments
     public HelloTVXlet() {
     }
     
     //initialization
-    public void initXlet(XletContext context)
-        throws XletStateChangeException {
+    public void initXlet(XletContext context) throws XletStateChangeException {
+        HScene scene=HSceneFactory.getInstance().getDefaultHScene();
+        // SCHERM = 720 x 576
+        
+        HStaticText hst=new HStaticText("Harry Potter Quiz",0,0,720,576); // tekst,x,y,w,h
+        hst.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        hst.setBackground(Color.BLACK);
+        scene.add(hst);
+        
+        scene.validate();
+        scene.setVisible(true);
     }
  
     //start
