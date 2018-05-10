1bba8d82f9769bb541c7d1ccd262842a5f1c363b
Katriena Banasik <katriena.0107@hotmail.com>
2018-05-10 14:56:57 +0200

    empty project

---
+++ src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,39 @@
+package hellotvxlet;
+
+import javax.tv.xlet.*;
+import org.havi.ui.*;
+import java.awt.Color;
+
+/**
+ *
+ * @author student
+ */
+
+public class HelloTVXlet implements Xlet { //implements javax.microedition.xlet.Xlet
+    
+    //default constructor without arguments
+    public HelloTVXlet() {
+    }
+    
+    //initialization
+    public void initXlet(XletContext context)
+        throws XletStateChangeException {
+    }
+ 
+    //start
+    public void startXlet() {
+        
+    }
+ 
+    //pauze (free resources, stop unnecessary threads, remove itself from the screen)
+    public void pauseXlet() {
+        
+    }
+ 
+    //destroy xlet
+    public void destroyXlet(boolean unconditional) throws XletStateChangeException {
+
+    }
+}
+
+
