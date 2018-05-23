4be3d97029d2de2acb250a34b9f908a8cd593fda
Katriena Banasik <katriena.0107@hotmail.com>
2018-05-23 15:02:26 +0200

    basic user input handler

--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -3,13 +3,16 @@ package hellotvxlet;
 import javax.tv.xlet.*;
 import org.havi.ui.*;
 import java.awt.Color;
+import java.awt.event.*;
+import org.dvb.event.*;
+import org.havi.ui.event.*;
 
 /**
  *
  * @author student
  */
 
-public class HelloTVXlet implements Xlet {
+public class HelloTVXlet implements Xlet, UserEventListener {
     
     //default constructor without arguments
     public HelloTVXlet() {
@@ -31,7 +34,19 @@ public class HelloTVXlet implements Xlet {
  
     //start
     public void startXlet() {
+        // EventManager aanvragen
+        EventManager manager = EventManager.getInstance();
         
+        // Repository
+        UserEventRepository repository = new UserEventRepository("Voorbeeld");
+        
+        // Events toevoegen
+        repository.addKey(org.havi.ui.event.HRcEvent.VK_RIGHT);
+        repository.addKey(org.havi.ui.event.HRcEvent.VK_LEFT);
+        repository.addKey(org.havi.ui.event.HRcEvent.VK_ENTER);
+        
+        // Bekend maken bij EventManager
+        manager.addUserEventListener((UserEventListener)this,repository );
     }
  
     //pauze (free resources, stop unnecessary threads, remove itself from the screen)
@@ -42,6 +57,28 @@ public class HelloTVXlet implements Xlet {
     //destroy xlet
     public void destroyXlet(boolean unconditional) throws XletStateChangeException {
 
+    }
+    
+    // --------------------------------------------------
+    
+    // Key Events Handler
+    public void userEventReceived(org.dvb.event.UserEvent e) {
+        //IF USER HITS ENTER
+        if(e.getType() == KeyEvent.KEY_PRESSED){
+            switch(e.getCode()) {
+                case HRcEvent.VK_RIGHT:
+                    System.out.println("RIGHT");
+                    break;
+                    
+                case HRcEvent.VK_LEFT:
+                    System.out.println("LEFT");
+                    break;
+                    
+                case HRcEvent.VK_ENTER:
+                    System.out.println("ENTER");
+                    break;
+            }
+        }
     }
 }
 
